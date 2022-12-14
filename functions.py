ebb14613127e9923e7d25966348a4112b7aeb67b
Terekhin <blackrat72@qip.ru>
2022-12-14 19:28:17 +0300

    add work with file

--- functions.py
+++ functions.py
@@ -23,3 +23,25 @@ def give_int_num(input_string: str,
             return num
         except ValueError:
             print('Похоже это не число, попробуте еще раз')
+
+
+def read_data_string(filename: str , coding:  str) -> str:
+
+    with open(filename, 'r', encoding=coding) as file:
+        data = file.read()
+    return data
+
+
+
+def write_string_to_file(filename: str , coding:  str, string: str):
+
+    with open(filename, 'w', encoding=coding) as file:
+        file.write(string)
+
+
+
+
+def add_string_to_file(filename: str , coding:  str, string: str):
+
+    with open(filename, 'a', encoding=coding) as file:
+        file.write(string)            
