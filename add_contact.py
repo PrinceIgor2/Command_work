4dbb933e1c9d3ab10ec8e8f8ab0c3c14fd1fb098
Ancha2702 <113132853+Ancha2702@users.noreply.github.com>
2022-12-15 15:39:57 +0300

    версия 2

--- add_contact.py
+++ add_contact.py
@@ -2,7 +2,7 @@ from functions import give_int_num, add_string_to_txt
 
 filename = 'phone_db.txt'
 
-def input_firstname():
+def input_firstname()->str:
     """_Ввод фамилии, если пользователь ввел фамилию
     с маленькой буквы- исправит на большую_
 
@@ -10,29 +10,30 @@ def input_firstname():
         _str_: _фамилия_
     """
     first = input( "Введите фамилию: ") 
-    remfname = first[1:] 
-    firstchar = first[0] 
-    return firstchar.upper() + remfname 
+    
+    if len(first) > 15:
+        input_firstname()
+    else: return first.title()
 
-def input_lastname(): 
-    last = input( "Введите имя: ") 
-    remlname = last[1:] 
-    firstchar = last[0] 
-    return firstchar.upper() + remlname
+def input_lastname()->str: 
+    last = input( "Введите имя: ")
+    if len(last) > 15:
+        input_lastname()
+    else: return last.title()
 
  
 def add(): 
-    """Создаем контакт
+    """_Добавление контакта в файл и проверка номера телефона на число и минимальное и максимальное количество символов_
     """
     firstname = input_firstname() 
     lastname = input_lastname() 
-    phone_number = give_int_num( "Номер телефона: ", min_num=100, max_num=99999999999) 
+    phone_number = give_int_num( "Номер телефона: ", min_num=100, max_num=99999999999) # проверка телефона 
     phone_number = str(phone_number)
     comment = input('Введите комментарий: ')
-    contact_details = f'{firstname} {lastname} {phone_number} {comment}'
-    add_string_to_txt(filename, 'UTF-8', contact_details)
+    contact_details = f'{firstname} {lastname} {phone_number} {comment}' #формируем итоговую строку
+    add_string_to_txt(filename, 'UTF-8', contact_details) #запись в файл 
     # myfile = open(filename, "a", encoding='UTF-8') 
     # myfile.write(contact_details) 
     print(f'Контакт:\n  {contact_details} сохранен!') 
 
-add()
+#add()
