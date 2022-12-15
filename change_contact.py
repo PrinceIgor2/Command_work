5714222597f434801372f99ed58d071031db0fa8
Kostya <kostilio911@gmail.ru>
2022-12-15 14:57:49 +0400

    change files

--- change_contact.py
+++ change_contact.py
@@ -1,37 +1,34 @@
-def change_contact():
+from user_interface import get_menu_item
+def change():
+    find,last,res = [],[],[]
     delete_item = input("Введите фамилию / фамилию и имя / номер телефона контакта, который нужно изменить\n> ")
-    find = []
-    last = []
-    with open('bd.txt', 'r') as file:
+    with open('bd.txt', 'r', encoding='utf-8') as file:
         for line in file:
             if delete_item.lower() not in line.lower():
                 last.append(line)
             else:
                 find.append(line)
-
     if len(find)>1:
         print(f"\nПод ваш запрос подходят несколько контактов\n", *find)
         print('Уточните свой запрос\n')
-        change_contact()
+        resp = input("Продолжить - 'да', Выйти в главное меню - 'любой символ':\n>")
+        change() if resp.lower() == 'да' else get_menu_item()
     elif len(find) == 0:
         print('Не удалось найти контакт\nУточните свой запрос\n')
-        change_contact()
+        resp = input("Продолжить - 'да', Выйти в главное меню - 'любой символ':\n>")
+        change() if resp.lower() == 'да' else get_menu_item()
     else:
         print(f"Выбран контакт для изменения\n", *find)
-        find[0]  = input("Введите новое значение для контакта: \n> ")
-        res = []
+        find[0] = input("Введите новое значение для контакта: \n> ")
         res = last+find
-        with open('bd.txt', 'w') as file:
+        with open('bd.txt', 'w', coding='utf-8') as file:
             for item in sorted(res):
                 if '\n' in item:
                     file.write(f'{item}')
                 else:
                     file.write(f'{item}\n')
-        print('Контакт успешно изменён\nХотите вернуться в главное меню?')
-        # Неплохо было бы после отфильтровать полученный список(только нужно опред,
-        #по какому принципу будем приводить в порядок(буква фамилии к примеру)
-        #Здесь будет проверка и возврат в главное меню
+        print('Контакт успешно изменён\n')
+        resp = input("Продолжить  - 'да', Выйти в главное меню - 'любой символ':\n>")
+        change() if resp.lower() == 'да' else get_menu_item()
 
-
-change_contact()
-
+change()
--- remove_contact.py
+++ remove_contact.py
@@ -1,47 +1,67 @@
-def delete_element():
-    new = []
-    kash_del = []
-    delete_item = input("Введите фамилию (фамилию и имя или номер телефона) контакта, который нужно удалить\n> ")
-    verify_delete = "да"
-    verify_delete  = input(f"Вы действительно хотите удалить контакт со значением >{delete_item}< ? \nНапишите 'да' или 'нет'\n> ")
+import os
+import time
+from user_interface import get_menu_item
+from sty import fg, bg, ef, rs
+def delete():
+    new,kash_del = [],[]
+    delete_item = input("Введите фамилию / фамилию и имя / номер телефона контакта, который нужно удалить\n> ")
+    verify_delete  = input(f"Вы действительно хотите удалить контакт со значением >{delete_item}< ? "
+                           f"\nНапишите 'да'/'нет'/'q'(выход в меню)\n> ")
     if verify_delete == "да":
-        with open('bd.txt', 'r') as file:
+        for  item  in read_data_string(bd.txt, 'UTF-8'):
+            for i in item:
+                if delete_item.lower()
+
+
+        with open('bd.txt', 'r', encoding = 'UTF-8') as file:
             for line in file:
                 if delete_item.lower() not in line.lower():
                     new.append(line)
                 else:
                     kash_del.append(line)
         if len(kash_del) == 0:
-            print('Не найдено контактов по заданным параметрам. Уточните свой запрос')
-            delete_element()
+            resp = input("Не найдено контактов по заданным параметрам. Хотите уточнить свой запрос?"
+                  "\nНапишите 'да'/'q'(выход в меню)(\n>")
+            delete() if resp.lower() == 'да' else get_menu_item()
+
         elif len(kash_del) == 1:
             with open('bd.txt', 'w') as file:
                 file.writelines(new)
             print('Контакт успешно удален')
         else:
-            print (f"Под ваш запрос подходят несколько контактов\n", *kash_del)
-            accept = input("Хотите удалить все выбранные контакты? да/нет \n> ")
+            print(f"Под ваш запрос подходят несколько контактов\n", *kash_del)
+            accept = input("Хотите удалить все выбранные контакты? 'да'/'нет'/'q'(выход в меню) \n> ")
             if accept.lower() == 'да':
-                with open('bd.txt', 'w') as file:
+                with open('bd.txt', 'w', encoding = 'UTF-8') as file:
                     file.writelines(new)
                 print('Контакты успешно удалены')
-            else:
+                time.sleep(2)
+                get_menu_item()
+            elif accept.lower() == 'нет':
                 print('Уточните свой запрос\n')
-                delete_element()
-
+                delete()
+            else:
+                get_menu_item()
+    elif verify_delete == "q":
+        get_menu_item()
     else:
-        delete_element()
-#Сюда можно еще допилить кнопку выхода в главное меню на любом из этапов, когда главное меню появится
+        delete()
 
-# delete_element()
+# delete()
 
-def delete_all_element():
-    print("\033[31m{}".format('Вы вошли в меню полного удаления всех контактов'))
+def delete_all():
+    print('Вы вошли в меню полного удаления всех контактов\n')
+
     resp_1 = input('Хотите продолжить?\nда/нет\n> ')
-    #если да, возврат в менюшку, нет продолжаем
+    if resp_1.lower() != 'да':
+        get_menu_item()
     resp_2 = input('Вы уверены что хотите удалить все контакты?\nда/нет\n> ')
-    #если да, возврат в менюшку, нет продолжаем и все удаляем
-    with open('bd.txt', 'w') as file:
-        file.writelines('')
+    if resp_2.lower() != 'да':
+        get_menu_item()
+    path = '/Command_work/bd.txt'
+    os.remove(path)
 
-# delete_all_element()
+delete()
+
+
+
