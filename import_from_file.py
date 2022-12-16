e47280e46857076431716b901f65b609b345b907
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-12-16 04:51:33 +0300

    corrected code to import from file

--- import_from_file.py
+++ import_from_file.py
@@ -4,15 +4,11 @@ from functions import read_from_txt
 def import_data(path_file_phone_db, coding):
     path_file_import = input(
         'Укажите путь файла откуда хотите импортировать контакты -> ')
-    while True:
-        try:
-            data = read_from_txt(path_file_import, coding)
-            with open(path_file_phone_db, 'a+', encoding=coding) as ph_db:
-                ph_db.write(f'\n')
-                for i in data:
-                    ph_db.write(f'{i}')
-            break
-        except:
-            print(
-                'Такого файла не существует. Проверьте и укажите корректный файл для импорта.')
-            break
+    try:
+        data = read_from_txt(path_file_import, coding)
+        with open(path_file_phone_db, 'a+', encoding=coding) as ph_db:
+            ph_db.write(f'\n')
+            for i in data:
+                ph_db.write(f'{i}')
+    except:
+        print('Такого файла не существует. Проверьте и укажите корректный файл для импорта.')
