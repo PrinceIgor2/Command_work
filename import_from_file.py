12bd1f2581328eca7d17bd230e62e91af4eb078c
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-12-16 03:57:30 +0300

    added code to import from file

---
+++ import_from_file.py
@@ -0,0 +1,18 @@
+from functions import read_from_txt
+
+def import_data(path_file_phone_db, coding):
+    path_file_import = input(
+        'Укажите путь файла откуда хотите импортировать контакты -> ')
+    while True:
+        try:
+            data = read_from_txt(path_file_import, coding)
+            with open(path_file_phone_db, 'a+', encoding=coding) as ph_db:
+                ph_db.write(f'\n')
+                for i in data:
+                    ph_db.write(f'{i}')
+            break
+        except:
+            print('Такого файла не существует. Проверьте и укажите корректный файл для импорта:')
+            break
+                        
+import_data('phone_db.txt', 'utf-8')
