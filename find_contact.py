e1ce9f79e8f8cee2dc4691f8f2b9245604ee872c
Terekhin <blackrat72@qip.ru>
2022-12-17 00:51:25 +0300

    добавлен возврат найденного списка, поправлен интерфейс

--- find_contact.py
+++ find_contact.py
@@ -1,44 +1,46 @@
 
 import sys
-from functions import read_from_txt, string_to_list
+from functions import read_from_txt, string_to_list, list_to_string
 
 
 def find():
-  file_path = 'phone_db.txt'
-  read_data = read_from_txt(file_path, 'UTF-8')
-  pb = string_to_list(read_data)
-  # Данная функция реализует поиск существующего контакта и выводит результат
-  choice = int(input("Выберите критерий поиска\n\n\1. Имя\n2. Фамилия\n3. Номер телефона\n4. Коммментарий)\
-  \n Пожалуйста, введите: "))
-
-  temp = []
- 
-  if choice == 1:
-  # Реализация поиска по имени контакта
-    query = str(input("Введите имя контакта для поиска: "))    
-
-  if choice == 2:    
-    # Реализация поиска по фамилии контакта
-    query = str(input("Введите фамилию контакта для поиска: "))        
-        
-  if choice == 3:
-    # Реализация поиска по номеру телефона контакта
-    query = int(input("Введите номер телефона контакта для поиска: "))
-            
-  if choice == 4:
-  # Реализация поиска по комментарию к контакту
-    query = str(input("Введите комментарий к контакту для поиска: "))
+    file_path = 'phone_db.txt'
+    read_data = read_from_txt(file_path, 'UTF-8')
+    pb = string_to_list(read_data)
     
-  for i in range(len(pb)):
-      if query == pb[i][choice - 1]:
-        temp.append(pb[i])        
+    # Данная функция реализует поиск существующего контакта и выводит результат
+    choice = int(input("\n1. Фамилия\n2. Имя\n3. Номер телефона\n4. Коммментарий)\
+  \nВыберите критерий поиска: >"))
+    query = ""
+    temp = []
 
-  else:
-    print("Неверный критерий поиска, дружок!")
+    if choice == 1:
+        # Реализация поиска по имени контакта
+        query = str(input("Введите фамилию контакта для поиска: "))
 
-    print(f"Попробуй поискать еще разок {string_to_list(temp)}")
+    elif choice == 2:
+        # Реализация поиска по фамилии контакта
+        query = str(input("Введите имя контакта для поиска: "))
 
+    elif choice == 3:
+        # Реализация поиска по номеру телефона контакта
+        query = int(input("Введите номер телефона контакта для поиска: "))
 
+    elif choice == 4:
+        # Реализация поиска по комментарию к контакту
+        query = str(input("Введите комментарий к контакту для поиска: "))
+    else:
+        print("Неверный критерий поиска, дружок!")
+        return
 
+    for i in range(len(pb)):
+        if query == pb[i][choice - 1]:
+            temp.append(pb[i])
+            
+    
+    if len(temp) == 0:
+        print(f"Попробуй поискать еще разок {list_to_string(pb)}")
+    else: 
+        print('Найдено:\n', list_to_string(temp))
 
-
+    return temp
