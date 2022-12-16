abd7b71ccb505c280025af9e23d1de2b1cc26630
Terekhin <blackrat72@qip.ru>
2022-12-16 23:12:47 +0300

    обсуждение с Игорем

--- find_contact.py
+++ find_contact.py
@@ -5,74 +5,68 @@
 # 3.добавил функцию"показать содержимое всей книги" / На всякий случай)))
 
 import sys
+from functions import read_from_txt, string_to_list
 
-def find_contact(pb):
+
+def find():
+	file_path = 'phone_db.txt'
+	sfgc = read_from_txt(file_path, 'UTF-8')
+	pb = string_to_list(sfgc)
 	# Данная функция реализует поиск существующего контакта и выводит результат
 	choice = int(input("Выберите критерий поиска\n\n\1. Имя\n2. Фамилия\n3. Номер телефона\n4. Коммментарий)\
 	\n Пожалуйста, введите: "))
-		
+
 	temp = []
 	check = -1
-	
+
 	if choice == 1:
 	# Реализация поиска по имени контакта
-		query = str(input("Введите имя контакта для поиска: "))
-		for i in range(len(pb)):
-			if query == pb[i][0]:
-				check = i
-				temp.append(pb[i])
+		query = str(input("Введите имя контакта для поиска: "))		
 
-    elif choice == 2: 
-        # Реализация поиска по фамилии контакта
-        query = str(input("Введите фамилию контакта для поиска: "))
-		for i in range(len(pb)):
-			if query == pb[i][1]:
-				check = i
-				temp.append(pb[i])
+	if choice == 2:    
+		# Реализация поиска по фамилии контакта
+		query = str(input("Введите фамилию контакта для поиска: "))        
 				
-	elif choice == 3:
-	# Реализация поиска по номеру телефона контакта
+	if choice == 3:
+		# Реализация поиска по номеру телефона контакта
 		query = int(input("Введите номер телефона контакта для поиска: "))
-		for i in range(len(pb)):
-			if query == pb[i][2]:
-				check = i
-				temp.append(pb[i])
-				
-	elif choice == 4:
+
+	if choice == 4:
 	# Реализация поиска по комментарию к контакту
 		query = str(input("Введите комментарий к контакту для поиска: "))
-		for i in range(len(pb)):
-			if query == pb[i][3]:
+		
+	for i in range(len(pb)):
+			if query == pb[i][choice - 1]:
 				check = i
-				temp.append(pb[i])
-		
-		
+				temp.append(pb[i])				
+
 	else:
-    # Если пользователь вводит любой другой символ, поиск не реализуется
-	    print("Неверный критерий поиска, дружок!")
-		return -1
+		print("Неверный критерий поиска, дружок!")
+
+		print("полныйц список через функцию list_to")		
+    	# Если пользователь вводит любой другой символ, поиск не реализуется	    	
+	# 	return -1
 	
-	if check == -1:
-        # Указывает на то, что критерию запроса не  было найдено соответствий
-	else:
-        return -1
+	# if check == -1:
+    #     # Указывает на то, что критерию запроса не  было найдено соответствий
+	# 	f = 7
+	# else:
+    #     return -1
 	
-	else:
-		display_all(temp)
-		return check
+	# else:
+	# 	display_all(temp)
+	# 	return check
 		
 
 
-# Функция показывает все содержимое телефонной книги
-def display_all(pb):
-    if not pb:
-	# Если функция будет вызвана после удаления всех контактов, появится следующее сообщение
-        print("Список контактов пуст: []")
-	else:
-		for i in range(len(pb)):
-		    print(pb[i])
-
-
+# # Функция показывает все содержимое телефонной книги
+# def display_all(pb):
+#     if not pb:
+# 		#Если функция будет вызвана после удаления всех контактов, появится следующее сообщение
+#         print("Список контактов пуст: []")	
+# 	else:
+# 		for i in range(len(pb)):
+# 		    print(pb[i])
 
 
 
