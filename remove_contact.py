6c8647942761cbb32b90f8aee78afbce93ab9935
Kostya <kostilio911@gmail.ru>
2022-12-18 23:58:51 +0400

    final edit with find function

--- remove_contact.py
+++ remove_contact.py
@@ -2,7 +2,8 @@ import os
 import time
 
 from functions import read_from_txt, string_to_list, list_to_string
-from functions import find_by_name, write_string_to_txt, add_string_to_txt
+from functions import write_string_to_txt, add_string_to_txt
+from find_contact import find
 
 def delete():
     '''
@@ -10,43 +11,22 @@ def delete():
         пользователем контактов
     '''
     path = 'phone_db.txt'
-    delete_item = input("Введите данные контакта, который нужно удалить\n> ")
-    verify_delete  = input(f"Вы действительно хотите удалить контакт со значением >{delete_item}< ? "
+    print("Вы вошли в меню удаления контактов")
+    find_data = find()
+    if len(find_data) == 0:
+        delete()
+    verify_delete  = input(f"Вы действительно хотите удалить контакт? "
                            f"\nНапишите 'да'/'нет'/'q'(выход в меню)\n> ")
     if verify_delete == "да":
         data = read_from_txt(path, 'UTF-8')
         data = string_to_list(data)
-        find_data = find_by_name(delete_item, data)
         data = [i for i in data if i not in find_data]
-        if len(find_data) == 0:
-            resp = input("Не найдено контактов по заданным параметрам. Хотите уточнить свой запрос?"
-                  "\nНапишите 'да'/'q'(выход в меню)(\n>")
-            if resp.lower() == 'да':
-                delete()
-
-        elif len(find_data) == 1:
-            print(*find_data)
-            data = list_to_string(data)
-            write_string_to_txt(path, 'UTF-8', data)
-            print('Контакт успешно удален')
-        elif len(find_data) > 1:
-            print(f"Под ваш запрос подходят несколько контактов\n", *find_data)
-            accept = input("Хотите удалить все выбранные контакты? 'да'/'нет'/'q'(выход в меню) \n> ")
-            if accept == 'да':
-                data = list_to_string(data)
-                add_string_to_txt(path, 'UTF-8', data)
-                print('Контакты успешно удалены')
-                time.sleep(2)
-            elif accept.lower() == 'нет':
-                print('Уточните свой запрос\n')
-                delete()
-            else:
-                pass
+        data = list_to_string(data)
+        write_string_to_txt(path, 'UTF-8', data)
+        print('Контакт успешно удален')
     else:
         pass
 
-if __name__ == '__main__':
-    delete()
 
 def delete_all():
     '''
@@ -60,7 +40,7 @@ def delete_all():
     resp_2 = input('Вы уверены что хотите удалить все контакты?\nда/нет\n> ')
     if resp_2.lower() != 'да':
         pass
-    path_1 = '/Command_work/bd.txt' # поменять на реальный
+    path_1 = '/Command_work/bd.txt'
     os.remove(path_1)
 
 
